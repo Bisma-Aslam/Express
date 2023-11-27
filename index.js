7711c36b16fcbfe5a160d3830b5de001d0e70a72
Bisma-Aslam <bismamalik2056@gmail.com>
2023-11-27 21:48:54 +0500

    Code

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const products = ["Washing Machine", "Air Conditioner", "Fan"];
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.get("/api/search", function (req, res) {
+  res.send("API Search");
+});
+app.get("/api/products", function (req, res) {
+  res.send(products);
+});
+app.get("/api/products/:index", function (req, res) {
+  if (!products[req.params.index])
+    return res.status(400).send("Product not found");
+  res.send(products[req.params.index]);
+});
+app.put("/api/products/:index", function (req, res) {
+  products[req.params.index] = req.body.name;
+  res.send(products[req.params.index]);
+});
+app.delete("/api/products/:index", function (req, res) {
+  products.splice(req.params.index, 1);
+  res.send(products);
+});
+app.post("/api/products", function (req, res) {
+  products.push(req.body.name);
+  res.send(products);
+});
+
+app.listen(3000);
